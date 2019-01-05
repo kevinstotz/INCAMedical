1570e8217ddec83ee3e0f09560bf8d517dea9321
kevinstotz <kevin_stotz@yahoo.com>
2019-01-04 20:36:32 -0500

    static files

--- API/settings/Base.py
+++ API/settings/Base.py
@@ -194,8 +194,19 @@ class Base(Configuration):
     # Absolute path to the directory static files should be collected to.
     # Don't put anything in this directory yourself; store your static files
     # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-    # Example: "/home/media/media.lawrence.com/static/"
 
+    STATIC_ROOT = join("/var", "www", "html", "incamedical", "INCAMedical_web", "media", )
+    # URL prefix for static files.
+    # Example: "http://media.lawrence.com/static/"
+    STATIC_URL = '/static/'
+    # Additional locations of static files
+    STATICFILES_DIRS = (
+        join("/var", "www", "html", "incamedical", "INCAMedical", "static", ),
+        # Put strings here, like "/home/html/static" or "C:/www/django/static".
+        # Always use forward slashes, even on Windows.
+        # Don't forget to use absolute paths, not relative paths.
+    )
 
-# from API.settings.Dev import Dev
 from API.settings.Prod import Prod
+# from API.settings.Dev import Dev
+print(Prod.STATIC_ROOT)
